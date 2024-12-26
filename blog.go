e1763937c21f6bc7924e0831b0bf0f46681b61be
Keshav Chakravarthy <keshav-c@users.noreply.github.com>
2024-12-26 19:01:45 +0530

    Basic blog as per tutorial in go.dev

--- blog.go
+++ blog.go
@@ -2,21 +2,69 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/error/", errorHandler)
+	log.Fatal(http.ListenAndServe(":4000", nil))
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := LoadPage(title)
 	if err != nil {
-		return nil, err
+		fmt.Fprintf(os.Stderr, "Failed to load page: %s\n", err.Error())
+		http.Redirect(w, r, "/error/not-found", http.StatusTemporaryRedirect)
+		return
 	}
-	return &Page{Title: title, Body: body}, nil
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := LoadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.Save()
+	if err != nil {
+		http.Redirect(w, r, "/error/internal-error", http.StatusTemporaryRedirect)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tName string, p *Page) {
+	t, err := template.ParseFiles("templates/" + tName + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, p)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	var responseCodes map[string]int = map[string]int{
+		"not-found":      http.StatusNotFound,
+		"internal-error": http.StatusInternalServerError,
+	}
+	eName := r.URL.Path[len("/error/"):]
+	t, _ := template.ParseFiles("templates/error/" + eName + ".html")
+	status := responseCodes[eName]
+	w.WriteHeader(status)
+	t.Execute(w, Page{})
 }
--- page.go
+++ page.go
@@ -7,7 +7,16 @@ type Page struct {
 	Body  []byte
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
+func (p *Page) Save() error {
+	filename := "./pages/" + p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func LoadPage(title string) (*Page, error) {
+	filename := "./pages/" + title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
